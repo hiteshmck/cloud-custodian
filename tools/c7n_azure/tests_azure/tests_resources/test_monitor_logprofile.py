ca8bb0593514c81a6fea1604b2578d9dbffacac0
Hitesh Jain <hitesh_jain@mckinsey.com>
2023-05-25 11:21:21 -0700

    incorporated PR feedback

--- tools/c7n_azure/tests_azure/tests_resources/test_monitor_logprofile.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_monitor_logprofile.py
@@ -6,15 +6,15 @@ from ..azure_common import BaseTest
 class MonitorLogprofileTest(BaseTest):
     def test_azure_monitor_logprofile_schema_validate(self):
             p = self.load_policy({
-                'name': 'test-azure-monitor-logprofile-schema-validate',
-                'resource': 'azure.monitor-logprofile'
+                'name': 'test-azure-monitor-log-profile-schema-validate',
+                'resource': 'azure.monitor-log-profile'
             }, validate=True)
             self.assertTrue(p)
 
     def test_find_by_name(self):
         p = self.load_policy({
-            'name': 'test-azure-azure-monitor-logprofile',
-            'resource': 'azure.monitor-logprofile',
+            'name': 'test-azure-azure-monitor-log-profile',
+            'resource': 'azure.monitor-log-profile',
             'filters': [
                 {'type': 'value',
                  'key': 'name',
